663cd569bf91945abb32b1c35f645711b048f429
Dmitrii Markin <markin-dmitrij@yandex.ru>
2024-10-10 17:42:16 +0300

    Подготовил data слой для работы с интернетом
    - Добавил сущности

---
+++ app/src/main/java/com/example/weatherapp/data/network/api/ApiService.kt
@@ -0,0 +1,26 @@
+package com.example.weatherapp.data.network.api
+
+import com.example.weatherapp.data.network.dto.CityDto
+import com.example.weatherapp.data.network.dto.WeatherCurrentDto
+import com.example.weatherapp.data.network.dto.WeatherForecastDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("current.json?key=3f1a6d716b7e440d8e7140209241010")
+    suspend fun loadCurrentWeather(
+        @Query("q") query: String
+    ): WeatherCurrentDto
+
+    @GET("forecast.json?key=3f1a6d716b7e440d8e7140209241010")
+    suspend fun loadForecast(
+        @Query("q") query: String,
+        @Query("days") daysCount: Int = 4,
+    ): WeatherForecastDto
+
+    @GET("search.json?key=3f1a6d716b7e440d8e7140209241010")
+    suspend fun searchCity(
+        @Query("q") query: String,
+    ): List<CityDto>
+}
--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -2,19 +2,34 @@ package com.example.weatherapp.presentation
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.example.weatherapp.data.network.api.ApiFactory
 import com.example.weatherapp.presentation.ui.theme.WeatherAppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val apiService = ApiFactory.apiService
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val currentWeather = apiService.loadCurrentWeather("London")
+            val forecast = apiService.loadForecast("London")
+            val cities = apiService.searchCity("London")
+
+            Log.d("!!!", "$currentWeather \n $forecast, $cities")
+        }
         setContent {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {innerPadding ->
